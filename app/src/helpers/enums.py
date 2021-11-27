f91c7e8c4dfe228cd712753c18a5b9392f3f64f3
Avdiienko Illia <avil48job@gmail.com>
2021-11-27 20:21:04 +0200

    style(enums): enums cleaned up

--- app/src/helpers/enums.py
+++ app/src/helpers/enums.py
@@ -12,11 +12,3 @@ class Roles(MyEnum):
     doctor = "doctor"
     moder = "moder"
     admin = "admin"
-
-    @classmethod
-    def generate_permissions(cls) -> list[dict]:
-        boolean_flags = ("can_manage_users_data", "can_manage_medical_info")
-        return [
-            {"name": role, **dict(zip(boolean_flags, [int(i) for i in f"{index:02b}"]))}
-            for index, role in enumerate(cls.values())
-        ]
