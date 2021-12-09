a482929d60ce1b54c8a2abc3934f79a0a4a8aaf7
Avdiienko Illia <avil48job@gmail.com>
2021-12-10 00:54:27 +0200

    style(configs): wording changed

--- app/configs.py
+++ app/configs.py
@@ -8,7 +8,7 @@ env.read_env()
 FLASK_ENV = env.str("FLASK_ENV", default="development")
 PROD_ENV = FLASK_ENV == "production"
 
-SECRET_KEY = env.str("SECRET_KEY", "don't you mind being this a secret key?")
+SECRET_KEY = env.str("SECRET_KEY", "don't you mind this being a secret key?")
 
 SQLALCHEMY_DATABASE_URI = env.str("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
 SQLALCHEMY_TRACK_MODIFICATIONS = env.bool("SQLALCHEMY_TRACK_MODIFICATIONS", False)
